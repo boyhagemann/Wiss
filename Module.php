8ebf050126f5ce7a05450df8b9437119e6723cb9
boyhagemann <boyhagemann@gmail.com>
2012-08-27 23:05:32 +0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -15,20 +15,36 @@ use Zend\ModuleManager\ModuleManager;
 
 class Module
 {
-
-	public function onBootstrap($e)
+    /**
+     *
+     * @param MvcEvent $e
+     */
+	public function onBootstrap(MvcEvent $e)
     {
-		$evm = $e->getApplication()->getEventManager();		
+        // Get the event manager
+        $app    = $e->getApplication();
+        $sm     = $app->getServiceManager();
+		$evm    = $app->getEventManager();	
+		$config = $app->getConfig();	
+        
+        // Hook in to the route event, where is determined
+        // which controller/action has to be used
 		$evm->attach(MvcEvent::EVENT_ROUTE, function($e) {
 				
-			$config = $e->getApplication()->getConfig();	
+            // Get the matched route and its name	
 			$route = $e->getRouteMatch();	
-			$current = $route->getMatchedRouteName();	
+			$currentRoute = $route->getMatchedRouteName();	
 				
-			// Go to the install page first if there is no valid database connection
-			if($current != 'install' && $config['doctrine']['connection']['orm_default']['params']['password'] == 'password') {
-				$sm = $e->getApplication()->getServiceManager();
+            // Check the db connection. Is the password still 'password'? Then the
+            // connection is not set yet.
+            $dbParams = $config['doctrine']['connection']['orm_default']['params'];
+            
+    		// Go to the install page first if there is no valid database connection
+            // Only go there is the current route is not 'install' already
+			if($currentRoute != 'install' && $dbParams['password'] == 'password') {
 				
+				// Change to route before it is dispatched. It now goes to the
+                // install action.
 				$route->setParam('controller', 'Wiss\Controller\Index');
 				$route->setParam('action', 'install');				
 			}
@@ -39,20 +55,28 @@ class Module
 				// Rewrite all incoming uri's to a single entry point
 				$route->setParam('controller', 'Wiss\Controller\PageContent');
 				$route->setParam('action', 'route');
-				$route->setParam('route', $current);			
-				
+				$route->setParam('route', $current);				
 			}
 
 		});
     }
 	
+    /**
+     *
+     * @param ModleManager $moduleManager
+     */
     public function init(ModuleManager $moduleManager)
     {
+        // If the Wiss module is being used in the application, then
+        // a special layout is used.
         $sharedEvents = $moduleManager->getEventManager()->getSharedManager();
         $sharedEvents->attach(__NAMESPACE__, 'dispatch', function($e) {
-            // This event will only be fired when an ActionController under the MyModule namespace is dispatched.
+            
+            // This event will only be fired when an ActionController u
+            // Under the Wiss namespace is dispatched.
             $controller = $e->getTarget();
             $controller->layout('wiss/layout/layout');
+            
         }, 100);
     }
 	
@@ -65,6 +89,8 @@ class Module
     {
         return array(
             'factories' => array(
+                
+                // Instantiate a Flash Messenger
                 'flashMessenger' => function($sm) {
 					$locator = $sm->getServiceLocator();
 					$messenger = $locator->get('Zend\Mvc\Controller\Plugin\FlashMessenger');
