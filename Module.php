0039d2f9c9788619f339c4d1de23291c4878ff69
boyhagemann <boy@swis.nl>
2012-08-27 18:58:10 +0200

    update layout

--- Module.php
+++ Module.php
@@ -11,6 +11,7 @@ namespace Wiss;
 
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\ModuleManager\ModuleManager;
 
 class Module
 {
@@ -44,7 +45,17 @@ class Module
 
 		});
     }
-
+	
+    public function init(ModuleManager $moduleManager)
+    {
+        $sharedEvents = $moduleManager->getEventManager()->getSharedManager();
+        $sharedEvents->attach(__NAMESPACE__, 'dispatch', function($e) {
+            // This event will only be fired when an ActionController under the MyModule namespace is dispatched.
+            $controller = $e->getTarget();
+            $controller->layout('wiss/layout/layout');
+        }, 100);
+    }
+	
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
